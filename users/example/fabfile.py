7f73214e2d9efe669a029941d69a6d632713eff8
Eric Wang <ericwang919@gmail.com>
2019-02-03 01:58:43 -0600

    testing

--- users/example/fabfile.py
+++ users/example/fabfile.py
@@ -3,7 +3,7 @@ from fab_docker_gpu.fdg import *
 
 REPO_FORK = 'mudphudwang'
 REPO_BRANCH = 'master'
-ENV_PATH = '/home/ewang/nas-project/code/.env'
+ENV_PATH = '.env'
 
 def test_build():
     run('docker build -t test /home/ewang/nas-project/code/docker/configs/example/ --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)"')
