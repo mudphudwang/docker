4bb04e36434aa1f3594265a03d55472e76c232a6
Eric Wang <ericwang919@gmail.com>
2019-02-04 17:35:15 -0600

    moved deployment to gpu-deploy repo

--- users/ewang/scripts/neuro2ml_ewang.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import logging
-from nsearch_experiments import neuro2ml
-logging.basicConfig(level=logging.INFO)
-
-neuro2ml.Neuro2MLDataset().populate(reserve_jobs=True)
--- users/example/scripts/hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-with open('/tmp/hello', 'w') as f:
-    f.write('hello')
